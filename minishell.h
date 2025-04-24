aeb0306842fce8889b67e0a96695e410bde31e14
Ambroise Berenger <aberenge@c1r4p11.42lehavre.fr>
2025-04-24 17:25:14 +0200

    .h file

--- minishell.h
+++ minishell.h
@@ -6,9 +6,17 @@
 /*   By: aberenge <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 17:18:27 by aberenge          #+#    #+#             */
-/*   Updated: 2025/04/24 17:20:06 by aberenge         ###   ########.fr       */
+/*   Updated: 2025/04/24 17:25:06 by aberenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+typedef struct s_redir
+{
+	char	*file;
+	int		type;
+	t_redir	*next;
+}	t_redir;
+
 
 typedef struct s_env
 {
@@ -20,5 +28,14 @@ typedef struct s_env
 
 typedef struct s_token
 {
-
+	char	*value;
+	int		type;
+	t_token	*next;
 }	t_token;
+
+typedef struct s_cmd
+{
+	char	*name;
+	char	*path;
+	char	**args;
+}	t_cmd;
